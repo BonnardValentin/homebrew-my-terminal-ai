ff1c51427f3494da522f7965d15abc370889cbe7
Bonnard Valentin <val@2501.ai>
2025-01-22 19:36:45 +0100

    Fix SHA256 for v0.1.0 bis

--- Formula/ai-plugin.rb
+++ Formula/ai-plugin.rb
@@ -2,7 +2,7 @@ class AiPlugin < Formula
     desc "A simple AI-powered terminal plugin"
     homepage "https://github.com/BonnardValentin/homebrew-my-terminal-ai"
     url "https://github.com/BonnardValentin/homebrew-my-terminal-ai/archive/refs/tags/v0.1.0.tar.gz"
-    sha256 "c39e915f843d8ad5e6eb06036f539607def220a9fa960708eb4021ec96a3e772"
+    sha256 "e9a34148222c6783400c4c91c32a46f802eb8280a8016bf9eb377a20618bc29b"
     license "MIT"
   
     depends_on "python@3.13" # Adjust version if needed
